de66d6e006f44ea7a3b1fba193f52b544b3d46f7
Florentino Navarro <ceacademy.web@gmail.com>
2023-02-03 12:09:16 -0600

    database add

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -10,67 +10,68 @@ import {
 export const createOrder = async (req, res) => {
   console.log('client: ' + PAYPAL_API_CLIENT);
   const telegram = req.body.telegram;
-  try {
-    const order = {
-      intent: 'CAPTURE',
-      purchase_units: [
-        {
-          amount: {
-            currency_code: 'USD',
-            value: '200',
-          },
-        },
-      ],
-      application_context: {
-        brand_name: 'CEACADEMY',
-        landing_page: 'NO_PREFERENCE',
-        user_action: 'PAY_NOW',
-        return_url: `${HOST}/capture-order/${telegram}`,
-        cancel_url: `${HOST}/cancel-payment`,
-      },
-    };
+  console.log(telegram);
+  // try {
+  //   const order = {
+  //     intent: 'CAPTURE',
+  //     purchase_units: [
+  //       {
+  //         amount: {
+  //           currency_code: 'USD',
+  //           value: '200',
+  //         },
+  //       },
+  //     ],
+  //     application_context: {
+  //       brand_name: 'CEACADEMY',
+  //       landing_page: 'NO_PREFERENCE',
+  //       user_action: 'PAY_NOW',
+  //       return_url: `${HOST}/capture-order/${telegram}`,
+  //       cancel_url: `${HOST}/cancel-payment`,
+  //     },
+  //   };
 
-    // format the body
-    const params = new URLSearchParams();
-    params.append('grant_type', 'client_credentials');
+  //   // format the body
+  //   const params = new URLSearchParams();
+  //   params.append('grant_type', 'client_credentials');
 
-    // Generate an access token
-    const {
-      data: { access_token },
-    } = await axios.post(
-      'https://api-m.sandbox.paypal.com/v1/oauth2/token',
-      params,
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-          username: PAYPAL_API_CLIENT,
-          password: PAYPAL_API_SECRET,
-        },
-      }
-    );
+  //   // Generate an access token
+  //   const {
+  //     data: { access_token },
+  //   } = await axios.post(
+  //     'https://api-m.sandbox.paypal.com/v1/oauth2/token',
+  //     params,
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //       },
+  //       auth: {
+  //         username: PAYPAL_API_CLIENT,
+  //         password: PAYPAL_API_SECRET,
+  //       },
+  //     }
+  //   );
 
-    console.log(access_token);
+  //   console.log(access_token);
 
-    // make a request
-    const response = await axios.post(
-      `${PAYPAL_API}/v2/checkout/orders`,
-      order,
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      }
-    );
+  //   // make a request
+  //   const response = await axios.post(
+  //     `${PAYPAL_API}/v2/checkout/orders`,
+  //     order,
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${access_token}`,
+  //       },
+  //     }
+  //   );
 
-    console.log(response.data);
+  //   console.log(response.data);
 
-    return res.json(response.data);
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json('Something goes wrong');
-  }
+  //   return res.json(response.data);
+  // } catch (error) {
+  //   console.log('error: ' + error.message);
+  //   return res.status(500).json('Something goes wrong');
+  // }
 };
 
 export const captureOrder = async (req, res) => {
